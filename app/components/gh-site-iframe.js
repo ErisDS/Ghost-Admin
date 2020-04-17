e488a42a4ea2f293e98e2a15d5ff264e6eb12f34
Hannah Wolfe <erisds@gmail.com>
2020-04-17 12:35:39 +0100

    Added a dumb button to change the iframe URL
    - iframe gets a ?p=1 query param in the url
    - the button changes 1 to something between 1 and 10
    - we'll use this on the frontend to fake changing settings

--- app/components/gh-site-iframe.js
+++ app/components/gh-site-iframe.js
@@ -7,12 +7,13 @@ export default Component.extend({
     tagName: '',
 
     didReceiveAttrs() {
+        console.log('received attrs');
         // reset the src attribute each time the guid changes - allows for
         // a click on the navigation item to reset back to the homepage
         if (this.guid !== this._lastGuid) {
             let iframe = document.querySelector('#site-frame');
             if (iframe) {
-                iframe.src = `${this.config.get('blogUrl')}/`;
+                iframe.src = this.siteUrl;
             }
         }
         this._lastGuid = this.guid;
--- app/controllers/site.js
+++ app/controllers/site.js
@@ -1,6 +1,25 @@
 import Controller from '@ember/controller';
 import {alias} from '@ember/object/computed';
+import {computed} from '@ember/object';
+import {inject as service} from '@ember/service';
 
 export default Controller.extend({
-    guid: alias('model')
+    config: service(),
+
+    previewValue: 1,
+
+    guid: alias('model'),
+
+    siteUrl: computed('config.blogUrl', 'previewValue', function () {
+        let url = this.get('config.blogUrl');
+        let withSlash = url.slice(-1) !== '/' ? `${url}/` : url;
+        return `${withSlash}?p=${this.previewValue}`;
+    }),
+
+    actions: {
+        changeSomething() {
+            let newValue = Math.floor(Math.random() * 10 + 1);
+            this.set('previewValue', newValue);
+        }
+    }
 });
